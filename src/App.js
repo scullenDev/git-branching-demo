0f67b7935f7f56564725625aa008485ba1792183
sisay2405 <abrehasisay04@gmail.comgit config --global user.name sisay2405egit config --global user.email abrehasisay04@gmail.com>
2022-04-20 12:56:56 -0400

    new branch created

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return <div className="App">Can I push this change now?</div>;
+  return <div className="App">Can I push this change now? 
+  This sisay i just creat new branch
+  </div>;
 }
 
 export default App;
